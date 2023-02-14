0095411ad9f8c788412e99bab4bf3650ceb8b27b
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2023-02-13 18:29:27 -0800

    Added docstrings and dedicated classes for SudokuNumbers and empty Cells

--- main.py
+++ main.py
@@ -4,30 +4,51 @@ from sys import stdin
 from itertools import chain
 
 
+class SudokuNumber:
+    def __init__(self, num: str):
+        self.number_string = num
+
+
+class EmptyCell:
+    pass
+
+
 def varGenerator():
+    """Generates unique numbers from 0 onward infinitely. There is
+    probably something built in for this in Python but it's slipping
+    my mind"""
     current_var = 0
     while True:
         current_var += 1
         yield current_var
 
 
-def encodeCell(cell: str) -> list[str] | list[None]:
+def encodeCell(cell: str) -> list[SudokuNumber] | list[EmptyCell]:
+    """Converts a cell into either a list of SudokuNumber (being the
+    number at that cell) or a list of EmptyCell indicating nothing was
+    found at that cell
+
+    The reason it returns a list of EmptyCell specifically is due to python
+    not letting me iterate over Optionals
+
+    """
     if cell == "":
-        return [None]
+        return [EmptyCell()]
     else:
-        return [c for c in cell]
+        return [SudokuNumber(c) for c in cell]
 
 
-def cnfEncodeLine(line: str) -> list[str | None]:
+def cnfEncodeLine(line: str) -> list[SudokuNumber | EmptyCell]:
+    "Convert a line into a list of nu"
     return list(chain(*map(encodeCell, line.rstrip().split("."))))
 
 
-def printBaseEncoding(encodedVersion: list[list[str | None]]):
-    # Every cell contains at least one number
-    # Iterator used to generate a new number every time next is called on
-    # it
+def printBaseEncoding(encodedVersion: list[list[SudokuNumber | EmptyCell]]):
+    # Every cell contains at least one number Iterator used to
+    # generate a new number every time next is called on it
     iter = varGenerator()
     possible_values = 9
+    # May have over done it on the list composition here
     base_encoding: list[list[list[str]]] = [
         [[str(next(iter)) for _ in range(possible_values)] + ["0"] for _ in row]
         for row in encodedVersion
@@ -42,9 +63,14 @@ def printBaseEncoding(encodedVersion: list[list[str | None]]):
             print(" ".join(cell))
 
 
-# TODO maybe only drop it if that actually is the case since they may
-# not feed us the exact same file
-# Drop the last line since it's just the eof character
 def main():
-    encodedVersion = [*map(cnfEncodeLine, stdin)][0:-1]
+    # TODO maybe only drop it if that actually is the case since they may
+    # not feed us the exact same file
+    # Drop the last line since it's just the eof character
+    input = [line for line in stdin][0:-1]
+    encodedVersion = [*map(cnfEncodeLine, input)]
     printBaseEncoding(encodedVersion)
+
+
+if __name__ == "__main__":
+    main()
