f91207bb25102f0429a225a015b0c685ecdd1d30
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2023-02-25 08:44:42 -0700

    small correction to ranges

--- sud2sat.py
+++ sud2sat.py
@@ -170,7 +170,7 @@ def noDupInRow():
     clauses = []
     for i in range(1, 10):
         for k in range(1, 10):
-            for j in range(1, 10):
+            for j in range(1, 9):
                 for l in range(j + 1, 10):
                     clauses.append(
                         [
@@ -186,7 +186,7 @@ def noDupInCol():
     clauses = []
     for j in range(1, 10):
         for k in range(1, 10):
-            for i in range(1, 10):
+            for i in range(1, 9):
                 for l in range(i + 1, 10):
                     clauses.append(
                         [
