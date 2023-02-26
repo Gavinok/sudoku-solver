e51f062c43664226b0db15cb7cd7a282bbed6903
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2023-02-25 19:07:52 -0700

    All versions of sud2sat are now tested and delays are added for accuracy

--- testharness.py
+++ testharness.py
@@ -1,47 +1,64 @@
 #!/usr/bin/env python3
 import io
+import time
+from os import times
 from sys import stdin
 from functools import reduce
 import subprocess
 
-results = []
-with open("p096_sudoku.txt", "r") as sudfile:
-    input = [line for line in sudfile.readlines()]
-    tests = [input[i : i + 9] for i in range(1, len(input), 10)]
-    for test in tests:
-        p = subprocess.run(
-            ["./sud2sat"],
-            stdout=subprocess.PIPE,
-            input="".join(test),
-            encoding="ascii",
-        )
-        with open("input.txt", "w") as text_file:
-            text_file.write(p.stdout)
+
+def benchmark(sud2satexe):
+    results = []
+    with open("p096_sudoku.txt", "r") as sudfile:
+        input = [line for line in sudfile.readlines()]
+        tests = [input[i : i + 9] for i in range(1, len(input), 10)]
+        for test in tests:
             p = subprocess.run(
-                ["minisat", "input.txt", "output.txt"],
+                [sud2satexe],
                 stdout=subprocess.PIPE,
+                input="".join(test),
                 encoding="ascii",
             )
-        results.append(
-            [
-                float(line.split(": ")[1].split(" ")[0])
-                for line in p.stdout.splitlines()
-                if line.startswith("C") or line.startswith("M")
-            ]
-        )
-        with open("output.txt", "r") as text_file:
-            p2 = subprocess.run(
-                ["./sat2sud"],
-                stdout=subprocess.PIPE,
-                input=text_file.read(),
-                encoding="ascii",
+            with open("input.txt", "w") as text_file:
+                text_file.write(p.stdout)
+                p = subprocess.run(
+                    ["minisat", "input.txt", "output.txt"],
+                    stdout=subprocess.PIPE,
+                    encoding="ascii",
+                )
+            results.append(
+                [
+                    float(line.split(": ")[1].split(" ")[0])
+                    for line in p.stdout.splitlines()
+                    if line.startswith("C") or line.startswith("M")
+                ]
             )
+            with open("output.txt", "r") as text_file:
+                p2 = subprocess.run(
+                    ["./sat2sud"],
+                    stdout=subprocess.PIPE,
+                    input=text_file.read(),
+                    encoding="ascii",
+                )
+            time.sleep(3)
 
-memres = list(map(lambda x: x[0], results))
-cpures = list(map(lambda x: x[1], results))
+    memres = list(map(lambda x: x[0], results))
+    cpures = list(map(lambda x: x[1], results))
 
-print("results are" + str(results))
-print("mem average are " + str(reduce(lambda a, b: a + b, memres) / len(results)))
-print("mem worst are " + str(reduce(max, memres)))
-print("cpu time average are " + str(reduce(lambda a, b: a + b, cpures) / len(results)))
-print("cpu time average are " + str(max(cpures)))
+    print("Results for " + sud2satexe)
+    print("results are" + str(results))
+    print("mem average are " + str(reduce(lambda a, b: a + b, memres) / len(results)))
+    print("mem worst are " + str(reduce(max, memres)))
+    print(
+        "cpu time average are " + str(reduce(lambda a, b: a + b, cpures) / len(results))
+    )
+    print("cpu time average are " + str(max(cpures)))
+
+
+benchmark("./sud2sat")
+time.sleep(5)
+benchmark("./sud2sat1")
+time.sleep(5)
+benchmark("./sud2sat2")
+time.sleep(5)
+benchmark("./sud2sat3")
