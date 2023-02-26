f8702d6f89629fd768df4818b6b5104022621291
Gavin Jaeger-Freeborn <gavinfreeborn@gmail.com>
2023-02-25 18:13:56 -0700

    testharness now collects statistics

--- testharness.py
+++ testharness.py
@@ -1,29 +1,47 @@
 #!/usr/bin/env python3
+import io
 from sys import stdin
+from functools import reduce
 import subprocess
 
-
-input = [line for line in stdin]
-tests = [input[i : i + 9] for i in range(1, len(input), 10)]
-for test in tests:
-    p = subprocess.run(
-        ["./sud2sat"],
-        stdout=subprocess.PIPE,
-        input="".join(test),
-        encoding="ascii",
-    )
-    with open("input.txt", "w") as text_file:
-        text_file.write(p.stdout)
-    p = subprocess.run(
-        ["minisat", "input.txt", "output.txt"],
-        stdout=subprocess.PIPE,
-        encoding="ascii",
-    )
-    with open("output.txt", "r") as text_file:
-        p2 = subprocess.run(
-            ["./sat2sud"],
+results = []
+with open("p096_sudoku.txt", "r") as sudfile:
+    input = [line for line in sudfile.readlines()]
+    tests = [input[i : i + 9] for i in range(1, len(input), 10)]
+    for test in tests:
+        p = subprocess.run(
+            ["./sud2sat"],
             stdout=subprocess.PIPE,
-            input=text_file.read(),
+            input="".join(test),
             encoding="ascii",
         )
-        print(p2.stdout)
+        with open("input.txt", "w") as text_file:
+            text_file.write(p.stdout)
+            p = subprocess.run(
+                ["minisat", "input.txt", "output.txt"],
+                stdout=subprocess.PIPE,
+                encoding="ascii",
+            )
+        results.append(
+            [
+                float(line.split(": ")[1].split(" ")[0])
+                for line in p.stdout.splitlines()
+                if line.startswith("C") or line.startswith("M")
+            ]
+        )
+        with open("output.txt", "r") as text_file:
+            p2 = subprocess.run(
+                ["./sat2sud"],
+                stdout=subprocess.PIPE,
+                input=text_file.read(),
+                encoding="ascii",
+            )
+
+memres = list(map(lambda x: x[0], results))
+cpures = list(map(lambda x: x[1], results))
+
+print("results are" + str(results))
+print("mem average are " + str(reduce(lambda a, b: a + b, memres) / len(results)))
+print("mem worst are " + str(reduce(max, memres)))
+print("cpu time average are " + str(reduce(lambda a, b: a + b, cpures) / len(results)))
+print("cpu time average are " + str(max(cpures)))
